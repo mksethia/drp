c1bf1a5073d6681b2059961a7a83a35d5fffaae5
Kavya Shrestha <Ks2923@ic.ac.uk>
2025-06-12 16:14:37 +0100

    images on main page to distinguish clubs

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,33 +1,57 @@
-// prisma/seed.ts
-
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 async function main() {
-
   await prisma.club.deleteMany();
 
   const clubsData = [
-    { name: 'Red Tigers Football Club',    sport: 'Football', level: 'Experienced',  longitude: 12.4924, latitude: 41.8902 },
-    { name: 'Blue Dolphins Football Club', sport: 'Football', level: 'Intermediate', longitude: 12.4964, latitude: 41.9028 },
-    { name: 'Golden Eagles Football Club', sport: 'Football', level: 'Open to All',  longitude: 12.4964, latitude: 41.9028 },
-    { name: 'Green Hornets Rugby Club',    sport: 'Rugby',    level: 'Beginner',     longitude: 12.4964, latitude: 41.9028 },
+    {
+      name: 'Red Tigers Football Club',
+      sport: 'Football',
+      level: 'Experienced',
+      longitude: 12.4924,
+      latitude: 41.8902,
+      imageUrl: '/images/red-tiger.png'
+    },
+    {
+      name: 'Blue Dolphins Football Club',
+      sport: 'Football',
+      level: 'Intermediate',
+      longitude: 12.4964,
+      latitude: 41.9028,
+      imageUrl: '/images/red-tiger.png'
+    },
+    {
+      name: 'Golden Eagles Football Club',
+      sport: 'Football',
+      level: 'Open to All',
+      longitude: 12.4964,
+      latitude: 41.9028,
+      imageUrl: '/images/red-tiger.png'
+    },
+    {
+      name: 'Green Hornets Rugby Club',
+      sport: 'Rugby',
+      level: 'Beginner',
+      longitude: 12.4964,
+      latitude: 41.9028,
+      imageUrl: '/images/red-tiger.png'
+    },
   ];
-
 
   await prisma.club.createMany({
     data: clubsData,
     skipDuplicates: true,
-  })
+  });
 
-  console.log(`✅ Inserted ${clubsData.length} clubs into the database.`)
+  console.log(`✅ Inserted ${clubsData.length} clubs into the database.`);
 }
 
 main()
   .catch((e) => {
-    console.error(e)
-    process.exit(1)
+    console.error(e);
+    process.exit(1);
   })
   .finally(async () => {
-    await prisma.$disconnect()
-  })
+    await prisma.$disconnect();
+  });
