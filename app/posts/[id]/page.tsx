3c48f59f816e2d418b34ffccf461b22723452f47
aj1723 <alan.jiang23@imperial.ac.uk>
2025-06-13 12:26:54 +0100

    Fix elite

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,6 +1,9 @@
 import prisma from "@/lib/prisma";
 import { notFound, redirect } from "next/navigation";
 
+import Image from "next/image";
+import { Star } from "lucide-react";
+
 export const dynamic = "force-dynamic";
 
 export default async function Post({
