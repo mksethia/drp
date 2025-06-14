802f8da1ec3ddf8fec495fe6f0c5459ba496f804
mannan <mks23@ic.ac.uk>
2025-06-14 17:40:28 +0100

    added todo list, removed app/not-found, removed CLUBS button from header

--- app/Header.tsx
+++ app/Header.tsx
@@ -13,12 +13,6 @@ export default function Header() {
           NEXTPLAY
         </Link>
         <div className="flex items-center space-x-4">
-          <Link 
-            href="/posts" 
-            className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition"
-          >
-            CLUBS
-          </Link>
           {session ? (
             <>
               <Link 
