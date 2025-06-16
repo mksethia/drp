401fa08f01a034dd2111eb3e895b06455e18e172
mannan <mks23@ic.ac.uk>
2025-06-16 16:41:25 +0100

    attempt 4

--- app/page.tsx
+++ app/page.tsx
@@ -74,7 +74,7 @@ export default async function Home(
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center py-24 px-8"
       style={{
-        backgroundImage: "url(https://www.pexels.com/search/sports%20field/)",
+        backgroundImage: "url('/images/background.jpg')",
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}>
