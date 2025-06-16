c1b01564bcb033cd24d476f0f797f86d768a6538
mannan <mks23@ic.ac.uk>
2025-06-16 22:06:14 +0100

    Rehashed filters and db

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -2,7 +2,6 @@ import prisma from "@/lib/prisma";
 import { notFound, redirect } from "next/navigation";
 
 //import Image from "next/image";
-import { Star } from "lucide-react";
 import Slideshow from "@/app/components/slideshow";
 
 export const dynamic = "force-dynamic";
